c588e5435ecd711c898414a964fd11e588939367
github username <lushiyun@outlook.com>
2020-08-26 00:15:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log('function component')} />
   </div>,
   document.getElementById('root')
 );
