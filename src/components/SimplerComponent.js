17f136a272fe6a91b23493dac1f164bac648abe2
anette87 <anettem.rivera@gmail.com>
2020-09-11 19:55:18 -0700

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
-// Code SimplerComponent Here
+import React from 'react';
+
+  var SimplerComponent = function SimplerComponent(props) {
+    return(
+        <div onClick={props.handleClick}>"I am just happy"</div>
+    );
+  }
+
+  export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
+import { VirtualConsole } from 'jsdom';
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick= 
+     {event => 
+     { return(alert("Not anymore"))}}/>
   </div>,
   document.getElementById('root')
 );
