0b0a7ebd6a01866752863ce18f7ea3d23f09333a
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-18 18:40:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => event.target.innerText += ' as a clam!'} />
   </div>,
   document.getElementById('root')
 );
